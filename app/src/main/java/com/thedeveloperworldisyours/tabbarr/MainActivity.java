5ad22f73ac730e96ccb8a82d8c866af325cb0650
CabezasGonzalezJavier <javiergonzalezcabezas@gmail.com>
2017-01-09 10:38:42 +0000

    without adding toolbar

--- app/src/main/java/com/thedeveloperworldisyours/tabbarr/MainActivity.java
+++ app/src/main/java/com/thedeveloperworldisyours/tabbarr/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.thedeveloperworldisyours.tabbarr.adapter.TabsAdapter;
 
@@ -15,13 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        getSupportActionBar().setElevation(0);
+
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
 
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-        }
 
         viewPager.setAdapter(new TabsAdapter(getSupportFragmentManager(), this));
         tabLayout.setupWithViewPager(viewPager);
